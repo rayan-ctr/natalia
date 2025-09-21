3241219df1bf93ebec580ff6351d134b89469d45
rayan-ctr <64763076+rayan-ctr@users.noreply.github.com>
2025-09-21 19:45:28 +0200

    Delete proyecto directory

--- proyecto/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Professional website loaded successfully.');
-
-    document.getElementById('giftButton').addEventListener('click', () => {
-        const message = document.getElementById('giftMessage');
-        message.style.display = 'block';
-    });
-});
